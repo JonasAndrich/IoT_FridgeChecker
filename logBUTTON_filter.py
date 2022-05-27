d50185c69d76eb5ef439531cdf105b224fe91798
Jonas <jonas.andrich@gmail.com>
2022-05-27 19:39:04 +0200

    Refactoring db path change included sleep time

--- logBUTTON_filter.py
+++ logBUTTON_filter.py
@@ -17,7 +17,7 @@ dbPath = "/home/ubuntu/fridgechecker/Database.db"
 
 # delay of state changes. State changes taking less than this are not recorded
 # Change as you think
-delay = datetime.timedelta(seconds=0.4)
+delay = datetime.timedelta(seconds=0.3)
 
 
 # log sensor data to sqlite3 database
@@ -29,16 +29,18 @@ def logdata(state):
     conn.commit()
     conn.close()
 
-
 # main function
 def main():
     prev_state = False
-    past = datetime.datetime.now()
-    time.sleep(0.1)
+
     while True:
         # accesses the state value of the GPIO pin
         state = GPIO.input(16)
+        past = datetime.datetime.now()
         print(state)
+
+        time.sleep(delay.total_seconds())
+
         # defines now
         now = datetime.datetime.now()
 
@@ -50,29 +52,26 @@ def main():
         if state == prev_state:
             continue
 
-
         # Checks if door has been opened (state == True) and if state change is below defined delay
-        elif state == True and tdelta > delay:
+        elif state == 1: #and tdelta > delay:
             print("The Door has been opened at {}".format(now))
             prev_state = state
-            past = now
-            state = 1
+            # past = now
+            # state = 1
             logdata(state)
 
         # Checks if door has been closed (state == True) and if state change is below defined delay
-        elif state == False and tdelta > delay:
+        elif state == 0:# and tdelta > delay:
             print("The Door has been closed at %s" % now)
             prev_state = state
-            past = now
-            state = 0
+            # past = now
+            # state = 0
             logdata(state)
-        else:
-            continue
+
+
+#        else:
+#            continue
 
 
 # is called when script is executed
 main()
-
-# Nettes Feature. 
-# https://realpython.com/python-send-email/
-# Sende eine Mail, wenn Kühlschrank länger als 5 Minuten offen ist.
